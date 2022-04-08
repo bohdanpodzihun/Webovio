2b8f71e0a1ba9dd41cae3ff286825c1b0d38529f
bohdanpodzihun <bohdanpodzihun@gmail.com>
2022-04-08 10:47:23 +0300

    created opacity for services

--- Webovio/js/script.js
+++ Webovio/js/script.js
@@ -1,3 +1,10 @@
 let headerIcon = $('.headerLogoBtn')
 headerIcon.hide().fadeIn(1000)
 
+let services = $('.servicesGridItem').each(function() {
+    $(this).hover(function() {
+        $(this).removeClass('opacity')
+    }, function() {
+        $(this).addClass('opacity')
+    })
+})

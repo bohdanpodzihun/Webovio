7910e4ba43ac7d22fd61528867946c62e460b2f5
bohdanpodzihun <bohdanpodzihun@gmail.com>
2022-04-09 22:52:38 +0300

    added some jquery, changed html and css for services button "read more"

--- Webovio/js/script.js
+++ Webovio/js/script.js
@@ -1,6 +1,12 @@
 let headerIcon = $('.headerLogoBtn')
 headerIcon.hide().fadeIn(1000)
 
+
+let firstPageP = $('.firstPageGrid')
+firstPageP.hide().slideDown(2000)
+
+
+
 let services = $('.servicesGridItem').each(function() {
     $(this).hover(function() {
         $(this).removeClass('opacity')

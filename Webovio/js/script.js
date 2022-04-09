151a02a599391774f4358464c1f17bee2c165ca0
bohdanpodzihun <bohdanpodzihun@gmail.com>
2022-04-09 23:41:55 +0300

    again changed buttons added JQuery for them, also need fixing

--- Webovio/js/script.js
+++ Webovio/js/script.js
@@ -5,12 +5,30 @@ headerIcon.hide().fadeIn(1000)
 let firstPageP = $('.firstPageGrid')
 firstPageP.hide().slideDown(2000)
 
-
-
 let services = $('.servicesGridItem').each(function() {
     $(this).hover(function() {
         $(this).removeClass('opacity')
     }, function() {
         $(this).addClass('opacity')
     })
+})
+
+let btnOne = $('#btnOne').click(function() {
+    $('#moreTextOne').slideToggle(500)
+    return false
+})
+
+let btnTwo = $('#btnTwo').click(function() {
+    $('#moreTextTwo').slideToggle(500)
+    return false
+})
+
+let btnThree = $('#btnThree').click(function() {
+    $('#moreTextThree').slideToggle(500)
+    return false
+})
+
+let btnFour = $('#btnFour').click(function() {
+    $('#moreTextFour').slideToggle(500)
+    return false
 })
